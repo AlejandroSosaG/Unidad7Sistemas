fc43dafa0d31d5fa03c7e530ee4309027e535bdd
AlejandroSosaG <93648834+AlejandroSosaG@users.noreply.github.com>
2023-10-26 11:14:16 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Ejercicio1.Models;
+using Ejercicio1.Models.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ejercicio1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            DateTime horaActual = DateTime.Now;
+            String mensaje = "";
+            if (horaActual.Hour < 12)
+            {
+                mensaje = "Buenos días";
+            }
+            else if (horaActual.Hour >= 12 && horaActual.Hour <20)
+            {
+                mensaje = "Buenas tardes";
+            }
+            else if (horaActual.Hour >= 20)
+            {
+                mensaje = "Buenas noches";
+            }
+            ViewData["Mensaje"] = mensaje;
+            ViewBag.horaActual = DateTime.Now.ToLongDateString();
+            clsPersona persona= new clsPersona();
+            persona.Nombre = "Alejandro";
+            persona.Apellido = "Sosa";
+            return View(persona);
+        }
+        
+    }
+}
---
+++ Ejercicio1/Models/DAL/listadoPersonas.cs
@@ -0,0 +1,13 @@
+﻿using Ejercicio1.Models.Entidades;
+
+namespace Ejercicio1.Models.DAL
+{
+    public class listadoPersonas : clsPersona
+    {
+        List<clsPersona> lista;
+        public listadoPersonas()
+        {
+            lista = new List<clsPersona>();
+        }
+    }
+}
